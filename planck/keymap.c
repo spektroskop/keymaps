9c539d424f18a94d8a3240550fa104322a300152
Alexander Stensrud <alex@yuki.no>
2020-05-10 11:03:36 +0200

    Extract some common patterns

--- planck/keymap.c
+++ planck/keymap.c
@@ -3,21 +3,15 @@
 #include "spektroskop.h"
 
 enum layers { 
-  _BASE_ENT_SPC, _BASE_SPC, _BASE_F_J, _BASE_Z_SLSH,
-  _SHIFT1, _SHIFT2, _SHIFT3,
+  _BASE_ENT_SPC, _BASE_SPC, _BASE_Z_SLSH, _BASE_F_J,
+  _SHIFT, _SHIFT_SYM1, _SHIFT_SYM2,
   _SYM1, _SYM2,
-  _SYM3, _SYM4,
-  _FUNC,
-  _LANG,
-  _MOUSE,
-  _SELECT,
+  _FUNC, _LANG, _MOUSE, _SELECT,
 };
 
 enum keycodes {
   XK_LANG = SAFE_RANGE,
   XK_SEL,
-  XK_SYM1,
-  XK_SYM2,
 };
 
 enum dances {
@@ -48,103 +42,103 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 #define MOUSE TG(_MOUSE)
 
-#define ENT_SFT LT(_SHIFT1, KC_ENT)
-#define SPC_SFT LT(_SHIFT2, KC_SPC)
-#define SPC_SF3 LT(_SHIFT3, KC_SPC)
+#define ENT_SFT LT(_SHIFT_SYM2, KC_ENT)
+#define ENT_SF3 LT(_SHIFT, KC_ENT)
+#define SPC_SFT LT(_SHIFT_SYM1, KC_SPC)
+#define SPC_SF3 LT(_SHIFT, KC_SPC)
 
 #define TAB_FUN LT(_FUNC, KC_TAB)
 
-#define ENT_SYM LT(_SYM3, KC_ENT)
-#define SPC_SYM LT(_SYM4, KC_SPC)
+#define TAB_SYM LT(_SYM1, KC_TAB)
+#define BSP_SYM LT(_SYM2, KC_BSPC)
+#define SYM1 MO(_SYM1)
+#define SYM2 MO(_SYM2)
+#define CTL_TAB LCTL_T(KC_TAB)
+#define CTL_BSP RCTL_T(KC_BSPC)
+
+#define ENT_SYM LT(_SYM1, KC_ENT)
+#define SPC_SYM LT(_SYM2, KC_SPC)
 
 #define TD_LANG TD(_TD_LANG)
 
+#define OS_SYM1 OSL(_SYM1)
+#define OS_SYM2 OSL(_SYM2)
+
 #define DF_1 DF(_BASE_ENT_SPC)
 #define DF_2 DF(_BASE_SPC)
-#define DF_3 DF(_BASE_F_J)
-#define DF_4 DF(_BASE_Z_SLSH)
+#define DF_3 DF(_BASE_Z_SLSH)
+#define DF_4 DF(_BASE_F_J)
+
+#define BASE_R1 KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P
+#define BASE_R2 KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    DF_1,    DF_2,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN
+#define BASE_R3 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    DF_3,    DF_4,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
+
+#define SYM_R1  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN
+#define SYM_R2  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    SFT_7,   KC_8,    KC_9,    KC_0
+#define SYM_R3  KC_GRV,  KC_QUOT, KC_BSLS, KC_MINS, KC_EQL,  _______, _______, KC_TILD, KC_DQUO, _______, _______, _______
+#define SYM_R4  XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, KC_PIPE, KC_UNDS, KC_PLUS
+
+#define SFT_R1  S_Q,     S_W,     S_E,     S_R,     S_T,     KC_LCBR, KC_RCBR, S_Y,     S_U,     S_I,     S_O,     S_P
+#define SFT_R2  S_A,     S_S,     S_D,     S_F,     S_G,     _______, _______, S_H,     S_J,     S_K,     S_L,     KC_COLN
+#define SFT_R3  S_Z,     S_X,     S_C,     S_V,     S_B,     _______, _______, S_N,     S_M,     KC_LT,   KC_GT,   KC_QUES
+
+#define ______________ARROWS______________ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+
+#define F_KEYS KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+
+
+#define LAYOUT_planck_grid_wrapper(...) LAYOUT_planck_grid(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE_ENT_SPC] = LAYOUT_planck_grid(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    DF_1,    DF_2,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    DF_3,    DF_4,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+  [_BASE_ENT_SPC] = LAYOUT_planck_grid_wrapper(
+    BASE_R1, BASE_R2, BASE_R3,
     TD_LANG, KC_LALT, KC_LCTL, ENT_SFT, TAB_FUN, GUI_ESC, GUI_ESC, KC_BSPC, SPC_SFT, KC_RCTL, KC_LALT, KC_LGUI
   ),
 
-  [_BASE_SPC] = LAYOUT_planck_grid(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    DF_1,    DF_2,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    DF_3,    DF_4,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-    TD_LANG, KC_LALT, KC_LCTL, ENT_SYM, TAB_FUN, GUI_ESC, GUI_ESC, KC_BSPC, SPC_SF3, KC_RCTL, KC_LALT, KC_LGUI
+  [_BASE_SPC] = LAYOUT_planck_grid_wrapper(
+    BASE_R1, BASE_R2, BASE_R3,
+    TD_LANG, KC_LALT, SYM1,    ENT_SF3, CTL_TAB, GUI_ESC, GUI_ESC, CTL_BSP, SPC_SF3, SYM2,    KC_LALT, KC_LGUI
   ),
 
-  [_BASE_F_J] = LAYOUT_planck_grid(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    KC_A,    KC_S,    KC_D,    SFT_F,   KC_G,    DF_1,    DF_2,    KC_H,    SFT_J,   KC_K,    KC_L,    KC_SCLN,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    DF_3,    DF_4,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-    TD_LANG, KC_LALT, KC_LCTL, ENT_SYM, TAB_FUN, GUI_ESC, GUI_ESC, KC_BSPC, SPC_SYM, KC_RCTL, KC_LALT, KC_LGUI
-  ),
-
-  [_BASE_Z_SLSH] = LAYOUT_planck_grid(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    DF_1,    DF_2,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+  [_BASE_Z_SLSH] = LAYOUT_planck_grid_wrapper(
+    BASE_R1, BASE_R2, 
     SFT_Z,   KC_X,    KC_C,    KC_V,    KC_B,    DF_2,    _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  SFT_SLS,
     TD_LANG, KC_LALT, KC_LCTL, ENT_SYM, TAB_FUN, GUI_ESC, GUI_ESC, KC_BSPC, SPC_SYM, KC_RCTL, KC_LALT, KC_LGUI
   ),
 
-  [_SHIFT1] = LAYOUT_planck_grid(
-    S_Q,     S_W,     S_E,     S_R,     S_T,     KC_LCBR, KC_RCBR, S_Y,     S_U,     S_I,     S_O,     S_P,
-    S_A,     S_S,     S_D,     S_F,     S_G,     _______, _______, S_H,     S_J,     S_K,     S_L,     KC_COLN,
-    S_Z,     S_X,     S_C,     S_V,     S_B,     _______, _______, S_N,     S_M,     KC_LT,   KC_GT,   KC_QUES,
-    XXXXXXX, _______, _______, _______, _______, _______, _______, _______, XK_SYM2, _______, _______, _______
+  [_BASE_F_J] = LAYOUT_planck_grid_wrapper(
+    BASE_R1, 
+    KC_A,    KC_S,    KC_D,    SFT_F,   KC_G,    DF_1,    DF_2,    KC_H,    SFT_J,   KC_K,    KC_L,    KC_SCLN,
+    BASE_R3,
+    TD_LANG, KC_LALT, KC_LCTL, ENT_SYM, TAB_FUN, GUI_ESC, GUI_ESC, KC_BSPC, SPC_SYM, KC_RCTL, KC_LALT, KC_LGUI
   ),
 
-  [_SHIFT2] = LAYOUT_planck_grid(
-    S_Q,     S_W,     S_E,     S_R,     S_T,     KC_LCBR, KC_RCBR, S_Y,     S_U,     S_I,     S_O,      S_P,
-    S_A,     S_S,     S_D,     S_F,     S_G,     _______, _______, S_H,     S_J,     S_K,     S_L,      KC_COLN,
-    S_Z,     S_X,     S_C,     S_V,     S_B,     _______, _______, S_N,     S_M,     KC_LT,   KC_GT,    KC_QUES,
-    XXXXXXX, _______, _______, XK_SYM1, _______, _______, _______, _______, _______, _______, _______,  _______
-  ),
-
-  [_SHIFT3] = LAYOUT_planck_grid(
-    S_Q,     S_W,     S_E,     S_R,     S_T,     KC_LCBR, KC_RCBR, S_Y,     S_U,     S_I,     S_O,      S_P,
-    S_A,     S_S,     S_D,     S_F,     S_G,     _______, _______, S_H,     S_J,     S_K,     S_L,      KC_COLN,
-    S_Z,     S_X,     S_C,     S_V,     S_B,     _______, _______, S_N,     S_M,     KC_LT,   KC_GT,    KC_QUES,
+  [_SHIFT] = LAYOUT_planck_grid_wrapper(
+    SFT_R1, SFT_R2, SFT_R3,
     XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______
   ),
 
-  [_SYM1] = LAYOUT_planck_grid(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    SFT_7,   KC_8,    KC_9,    KC_0,
-    KC_GRV,  KC_QUOT, KC_BSLS, KC_MINS, KC_EQL,  _______, _______, KC_TILD, KC_DQUO, KC_COMM, KC_DOT,  SFT_SLS,
-    XXXXXXX, _______, _______, XK_SYM1, _______, _______, _______, _______, _______, KC_PIPE, KC_UNDS, KC_PLUS
+  [_SHIFT_SYM1] = LAYOUT_planck_grid_wrapper(
+    SFT_R1, SFT_R2, SFT_R3,
+    XXXXXXX, _______, _______, OS_SYM1, _______, _______, _______, _______, _______, _______, _______,  _______
   ),
 
-  [_SYM2] = LAYOUT_planck_grid(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    SFT_7,   KC_8,    KC_9,    KC_0,
-    KC_GRV,  KC_QUOT, KC_BSLS, KC_MINS, KC_EQL,  _______, _______, KC_TILD, KC_DQUO, KC_COMM, KC_DOT,  SFT_SLS,
-    XXXXXXX, _______, _______, _______, _______, _______, _______, _______, XK_SYM2, KC_PIPE, KC_UNDS, KC_PLUS
+  [_SHIFT_SYM2] = LAYOUT_planck_grid_wrapper(
+    SFT_R1, SFT_R2, SFT_R3,
+    XXXXXXX, _______, _______, _______, _______, _______, _______, _______, OS_SYM2, _______, _______, _______
   ),
 
-  [_SYM3] = LAYOUT_planck_grid(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    SFT_7,   KC_8,    KC_9,    KC_0,
-    KC_GRV,  KC_QUOT, KC_BSLS, KC_MINS, KC_EQL,  _______, _______, KC_TILD, KC_DQUO, _______, _______, _______,
-    XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, KC_PIPE, KC_UNDS, KC_PLUS
+  [_SYM1] = LAYOUT_planck_grid_wrapper(
+    SYM_R1, SYM_R2, SYM_R3, SYM_R4
   ),
 
-  [_SYM4] = LAYOUT_planck_grid(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    SFT_7,   KC_8,    KC_9,    KC_0,
-    KC_GRV,  KC_QUOT, KC_BSLS, KC_MINS, KC_EQL,  _______, _______, KC_TILD, KC_DQUO, _______, _______, _______,
-    XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, KC_PIPE, KC_UNDS, KC_PLUS
+  [_SYM2] = LAYOUT_planck_grid_wrapper(
+    SYM_R1, SYM_R2, SYM_R3, SYM_R4
   ),
 
   [_FUNC] = LAYOUT_planck_grid(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
+    F_KEYS,
+    XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, _______, ______________ARROWS______________, XXXXXXX,
     XK_SEL,  XXXXXXX, DSK_1,   DSK_2,   DSK_3,   _______, _______, XXXXXXX, MOUSE,   XXXXXXX, XXXXXXX, KC_RSFT,
     XXXXXXX, _______, _______, _______, _______, RESET,   RESET,   _______, _______, _______, _______, _______
   ),
@@ -158,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SELECT] = LAYOUT_planck_grid(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, ______________ARROWS______________, XXXXXXX,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   ),
@@ -201,8 +195,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   switch (get_highest_layer(state)) {
     case _SYM1: set_led_levels(32, 0); break;
     case _SYM2: set_led_levels(0, 32); break;
-    case _SYM3: set_led_levels(32, 0); break;
-    case _SYM4: set_led_levels(0, 32); break;
     case _FUNC: set_led_levels(32, 32); break;
     case _MOUSE: set_led_levels(32, 32); break;
     default: set_led_levels(0, 0); break;
@@ -213,20 +205,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case XK_SYM1:
-      if (record->event.pressed) {
-        layer_invert(_SYM1);
-      }
-
-      return false;
-
-    case XK_SYM2:
-      if (record->event.pressed) {
-        layer_invert(_SYM2);
-      }
-
-      return false;
-
     case XK_LANG:
       if (record->event.pressed) {
         lang_on();
