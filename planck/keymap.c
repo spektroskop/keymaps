9f473824a1d5425c2be9ba5d3056383d7e1d4695
Alexander Stensrud <alex@yuki.no>
2020-05-12 10:41:11 +0200

    Alignment

--- planck/keymap.c
+++ planck/keymap.c
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, EUR_AE,  _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, EUR_OE,  _______,
         EUR_AA,  _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______,
-        __zzz__, _______, _______, KC_ENT,  KC_TAB,  LANG,    LANG,    _______, KC_SPC, _______, _______, XXXXXXX
+        __zzz__, _______, _______, KC_ENT,  KC_TAB,  LANG,    LANG,    _______, KC_SPC,  _______, _______, XXXXXXX
     ),
 };
 
